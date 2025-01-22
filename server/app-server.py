2cd1cd980d2334775000678e6610bb39de93bf2f
Sawyer Theis <sawyer.c.theis@gmail.com>
2025-01-21 22:13:00 -0600

    Add server and client implementations for robot communication

---
+++ server/app-server.py
@@ -0,0 +1,86 @@
+#!/usr/bin/env python3
+
+import selectors
+import socket
+import threading
+import traceback
+
+import libserver as libserver
+
+
+class TransThread:
+    _instance = None
+
+    # When a new instance is created, sets it to the same global instance
+    def __new__(cls):
+        # If the instance is None, create a new instance
+        # Otherwise, return already created instance
+        if cls._instance is None:
+            cls._instance = super().__new__(cls)
+            cls._instance._start()
+        return cls._instance
+    
+    def _start(self):
+        self.sel = selectors.DefaultSelector()
+        self.sensor_data = {"IMU": (0.0, 0.0, 0.0)}
+        self.robot_state = {"horizontal_motors": (0, 0, 0, 0), "vertical_motors": (0, 0), "enabled": False}
+        
+        self.host = 'localhost'
+        self.port = 65432
+    
+    def set_IMU_data(self, x : float, y : float, z : float):
+        self.sensor_data["IMU"] = (x, y, z)
+        
+    def get_horizontal_motors(self):
+        return self.robot_state["horizontal_motors"]
+    
+    def get_vertical_motors(self):
+        return self.robot_state["vertical_motors"]
+    
+    def get_enabled(self):
+        return self.robot_state["enabled"]
+
+    def begin_thread(self):
+        thread = threading.Thread(target=self.run_server_socket)
+        thread.start()
+
+    def accept_wrapper(self, sock):
+        conn, addr = sock.accept()  # Should be ready to read
+        print(f"Accepted connection from {addr}")
+        conn.setblocking(False)
+        message = libserver.Message(self.sel, conn, addr, self.robot_state, self.sensor_data)
+        self.sel.register(conn, selectors.EVENT_READ, data=message)
+    
+    def run_server_socket(self):
+        lsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        lsock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        lsock.bind((self.host, self.port))
+        lsock.listen()
+        print(f"Listening on {(self.host, self.port)}")
+        lsock.setblocking(False)
+        self.sel.register(lsock, selectors.EVENT_READ, data=None)
+        
+        try:
+            while True:
+                events = self.sel.select(timeout=None)
+                for key, mask in events:
+                    if key.data is None:
+                        self.accept_wrapper(key.fileobj)
+                    else:
+                        message = key.data
+                        try:
+                            message.process_events(mask, self.sensor_data)
+                            print(f"Received: {message.robot_state}")
+                        except Exception:
+                            print(
+                                f"Main: Error: Exception for {message.addr}:\n"
+                                f"{traceback.format_exc()}"
+                            )
+                            message.close()
+        except KeyboardInterrupt:
+            print("Caught keyboard interrupt, exiting")
+        finally:
+            self.sel.close()
+
+transmission = TransThread()
+transmission.begin_thread()
---
+++ server/libserver.py
@@ -0,0 +1,201 @@
+import io
+import json
+import selectors
+import struct
+import sys
+
+
+class Message:
+    def __init__(self, selector, sock, addr, default_robot_state, default_sensor_data):
+        self.selector = selector
+        self.sock = sock
+        self.addr = addr
+        self._recv_buffer = b""
+        self._send_buffer = b""
+        self._jsonheader_len = None
+        self.jsonheader = None
+        self.request = None
+        self.response_created = False
+        
+        self.sensor_data = default_sensor_data
+        self.robot_state = default_robot_state
+
+    def _set_selector_events_mask(self, mode):
+        """Set selector to listen for events: mode is 'r', 'w', or 'rw'."""
+        if mode == "r":
+            events = selectors.EVENT_READ
+        elif mode == "w":
+            events = selectors.EVENT_WRITE
+        elif mode == "rw":
+            events = selectors.EVENT_READ | selectors.EVENT_WRITE
+        else:
+            raise ValueError(f"Invalid events mask mode {mode!r}.")
+        self.selector.modify(self.sock, events, data=self)
+
+    def _read(self):
+        try:
+            # Should be ready to read
+            data = self.sock.recv(4096) # type: ignore
+        except BlockingIOError:
+            # Resource temporarily unavailable (errno EWOULDBLOCK)
+            pass
+        else:
+            if data:
+                self._recv_buffer += data
+            else:
+                raise RuntimeError("Peer closed.")
+
+    def _write(self):
+        if self._send_buffer:
+            print(f"Sending {self._send_buffer!r} to {self.addr}")
+            try:
+                # Should be ready to write
+                sent = self.sock.send(self._send_buffer) # type: ignore
+            except BlockingIOError:
+                # Resource temporarily unavailable (errno EWOULDBLOCK)
+                pass
+            else:
+                self._send_buffer = self._send_buffer[sent:]
+
+
+    def _json_encode(self, obj, encoding):
+        return json.dumps(obj, ensure_ascii=False).encode(encoding)
+
+    def _json_decode(self, json_bytes, encoding):
+        tiow = io.TextIOWrapper(
+            io.BytesIO(json_bytes), encoding=encoding, newline=""
+        )
+        obj = json.load(tiow)
+        tiow.close()
+        return obj
+
+    def _create_message(
+        self, *, content_bytes, content_type, content_encoding
+    ):
+        jsonheader = {
+            "byteorder": sys.byteorder,
+            "content-type": content_type,
+            "content-encoding": content_encoding,
+            "content-length": len(content_bytes),
+        }
+        jsonheader_bytes = self._json_encode(jsonheader, "utf-8")
+        message_hdr = struct.pack(">H", len(jsonheader_bytes))
+        message = message_hdr + jsonheader_bytes + content_bytes
+        return message
+
+    def _create_response_json_content(self):
+        content = self.sensor_data
+        content_encoding = "utf-8"
+        response = {
+            "content_bytes": self._json_encode(content, content_encoding),
+            "content_type": "text/json",
+            "content_encoding": content_encoding,
+        }
+        return response
+
+    def process_events(self, mask, sensor_data):
+        self.sensor_data = sensor_data
+        
+        if mask & selectors.EVENT_READ:
+            self.read()
+        if mask & selectors.EVENT_WRITE:
+            self.write()
+            
+        return self.robot_state
+
+    def read(self):
+        self._read()
+
+        if self._jsonheader_len is None:
+            self.process_protoheader()
+
+        if self._jsonheader_len is not None:
+            if self.jsonheader is None:
+                self.process_jsonheader()
+
+        if self.jsonheader:
+            if self.request is None:
+                self.process_request()
+
+    def write(self):
+        if self.request:
+            if not self.response_created:
+                self.create_response()
+
+        self._write()
+
+    def close(self):
+        print(f"Closing connection to {self.addr}")
+        try:
+            self.selector.unregister(self.sock)
+        except Exception as e:
+            print(
+                f"Error: selector.unregister() exception for "
+                f"{self.addr}: {e!r}"
+            )
+
+        try:
+            self.sock.close() # type: ignore
+        except OSError as e:
+            print(f"Error: socket.close() exception for {self.addr}: {e!r}")
+        finally:
+            # Delete reference to socket object for garbage collection
+            self.sock = None
+
+    def process_protoheader(self):
+        hdrlen = 2
+        if len(self._recv_buffer) >= hdrlen:
+            self._jsonheader_len = struct.unpack(
+                ">H", self._recv_buffer[:hdrlen]
+            )[0]
+            self._recv_buffer = self._recv_buffer[hdrlen:]
+
+    def process_jsonheader(self):
+        hdrlen = self._jsonheader_len
+        if len(self._recv_buffer) >= hdrlen: # type: ignore
+            self.jsonheader = self._json_decode(
+                self._recv_buffer[:hdrlen], "utf-8"
+            )
+            self._recv_buffer = self._recv_buffer[hdrlen:]
+            for reqhdr in (
+                "byteorder",
+                "content-length",
+                "content-type",
+                "content-encoding",
+            ):
+                if reqhdr not in self.jsonheader:
+                    raise ValueError(f"Missing required header '{reqhdr}'.")
+
+    def process_request(self):
+        content_len = self.jsonheader["content-length"] # type: ignore
+        if not len(self._recv_buffer) >= content_len:
+            return
+        data = self._recv_buffer[:content_len]
+        self._recv_buffer = self._recv_buffer[content_len:]
+        if self.jsonheader["content-type"] == "text/json": # type: ignore
+            encoding = self.jsonheader["content-encoding"] # type: ignore
+            self.request = self._json_decode(data, encoding)
+            print(f"Received request {self.request!r} from {self.addr}")
+            
+            self.robot_state = dict(self.request)
+        else:
+            raise ValueError(f"Unsupported content type: {self.jsonheader['content-type']!r}") # type: ignore
+        # Set selector to listen for write events, we're done reading.
+        self._set_selector_events_mask("w")
+
+    def create_response(self):
+        if self.jsonheader["content-type"] == "text/json": # type: ignore
+            response = self._create_response_json_content()
+        else:
+            # Binary or unknown content-type
+            raise ValueError(f"Unsupported content type: {self.jsonheader['content-type']!r}") # type: ignore
+        message = self._create_message(**response)
+        self.response_created = True
+        self._send_buffer += message
+        
+        self._set_selector_events_mask("r")
+        self._jsonheader_len = None
+        self.jsonheader = None
+        self.request = None
+        self.response_created = False
+        
