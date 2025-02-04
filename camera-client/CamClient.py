cca6ab7991a092af6072e09a371d131be45f71d9
Sawyer <sawyer.c.theis@gmail.com>
2025-02-04 16:02:06 -0600

    functioning classes but threading doesn't work

--- camera-client/CamClient.py
+++ camera-client/CamClient.py
@@ -1,46 +1,66 @@
-# lets make the client code
 import socket, cv2, pickle, struct
+import threading
 from tkinter import Tk, Label
 from PIL import Image, ImageTk
 
-# create socket
-client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-host_ip = 'localhost' # paste your server ip address here
-port = 9999
-client_socket.connect((host_ip, port)) # a tuple
-data = b""
-payload_size = struct.calcsize("Q")
+class VideoClient:
+    def __init__(self, host_ip, port):
+        self.client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.client_socket.connect((host_ip, port))
+        self.data = b""
+        self.payload_size = struct.calcsize("Q")
+        
+    def receive_frame(self):
+        while True:
+            while len(self.data) < self.payload_size:
+                packet = self.client_socket.recv(4*1024) # 4K
+                if not packet:
+                    return None
+                self.data += packet
+            packed_msg_size = self.data[:self.payload_size]
+            self.data = self.data[self.payload_size:]
+            msg_size = struct.unpack("Q", packed_msg_size)[0]
+            
+            while len(self.data) < msg_size:
+                self.data += self.client_socket.recv(4*1024)
+            
+            frame_data = self.data[:msg_size]
+            self.data = self.data[msg_size:]
+            frame = pickle.loads(frame_data)
+            return frame
 
-# Initialize Tkinter
-root = Tk()
-root.title("Receiving Video")
-label = Label(root)
-label.pack()
+    def close(self):
+        self.client_socket.close()
 
-def update_frame():
-    global data
-    while len(data) < payload_size:
-        packet = client_socket.recv(4*1024) # 4K
-        if not packet: 
-            root.quit()
+class VideoDisplay:
+    def __init__(self, video_client):
+        self.video_client = video_client
+        self.root = Tk()
+        self.root.title("Receiving Video")
+        self.label = Label(self.root)
+        self.label.pack()
+        self.update_frame()
+
+    def update_frame(self):
+        frame = self.video_client.receive_frame()
+        if frame is None:
+            print("No frame received. Exiting ...")
+            self.root.quit()
             return
-        data += packet
-    packed_msg_size = data[:payload_size]
-    data = data[payload_size:]
-    msg_size = struct.unpack("Q", packed_msg_size)[0]
-    
-    while len(data) < msg_size:
-        data += client_socket.recv(4*1024)
-  
-    frame_data = data[:msg_size]
-    data = data[msg_size:]
-    frame = pickle.loads(frame_data)
-    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-    img = Image.fromarray(frame)
-    imgtk = ImageTk.PhotoImage(image=img)
-    label.image = imgtk
-    label.configure(image=imgtk)
-    label.after(10, update_frame)
+        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        img = Image.fromarray(frame)
+        imgtk = ImageTk.PhotoImage(image=img)
+        self.label.image = imgtk
+        self.label.configure(image=imgtk)
+        self.label.after(10, self.update_frame)
 
-update_frame()
-root.mainloop()
+    def start(self):
+        self.root.mainloop()
+
+if __name__ == "__main__":
+    host_ip = 'localhost' # paste your server ip address here
+    port = 9999
+    video_client = VideoClient(host_ip, port)
+    video_display = VideoDisplay(video_client)
+    video_display.start()
+    video_client.close()
--- camera-server/CamServer.py
+++ camera-server/CamServer.py
@@ -3,38 +3,44 @@
 # This code is for the server 
 # Lets import the libraries
 import socket, cv2, pickle, struct, imutils
+import threading
 
-# Socket Create
-server_socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-host_name  = socket.gethostname()
-host_ip = socket.gethostbyname(host_name)
-host_ip = 'localhost'
-print('HOST IP:',host_ip)
-port = 9999
-socket_address = (host_ip,port)
+class VideoServer:
+    def __init__(self, host_ip, port):
+        self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.server_socket.bind((host_ip, port))
+        self.server_socket.listen(5)
+        print("LISTENING AT:", (host_ip, port))
+    
+    def start_thread(self):
+        thread = threading.Thread(target=self._start)
+        thread.daemon = False
+        thread.start()
 
-# Socket Bind
-server_socket.bind(socket_address)
+    def _start(self):
+        while True:
+            client_socket, addr = self.server_socket.accept()
+            print('GOT CONNECTION FROM:', addr)
+            if client_socket:
+                self.send_video(client_socket)
 
-# Socket Listen
-server_socket.listen(5)
-print("LISTENING AT:",socket_address)
-
-# Socket Accept
-while True:
-    client_socket,addr = server_socket.accept()
-    print('GOT CONNECTION FROM:',addr)
-    if client_socket:
+    def send_video(self, client_socket):
         vid = cv2.VideoCapture(0)
-        
-        while(vid.isOpened()):
-            img,frame = vid.read()
-            frame = imutils.resize(frame,width=320)
+        while vid.isOpened():
+            img, frame = vid.read()
+            frame = imutils.resize(frame, width=350)
             a = pickle.dumps(frame)
-            message = struct.pack("Q",len(a))+a
+            message = struct.pack("Q", len(a)) + a
             client_socket.sendall(message)
             
-            cv2.imshow('TRANSMITTING VIDEO',frame)
+            cv2.imshow('TRANSMITTING VIDEO', frame)
             key = cv2.waitKey(1) & 0xFF
-            if key ==ord('q'):
+            if key == ord('q'):
                 client_socket.close()
+                break
+
+if __name__ == "__main__":
+    host_ip = 'localhost'
+    port = 9999
+    video_server = VideoServer(host_ip, port)
+    video_server.start_thread()
