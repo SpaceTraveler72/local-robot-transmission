9beaebf93f901e97d7e57235c19bc9033c7fea23
Sawyer <sawyer.c.theis@gmail.com>
2025-02-10 19:47:08 -0600

    Refactor CamClient and CamServer to improve video streaming; replace class-based structure with streamlined functions for handling connections and frames

--- camera-client/CamClient.py
+++ camera-client/CamClient.py
@@ -1,66 +1,33 @@
-import socket, cv2, pickle, struct
-import threading
-from tkinter import Tk, Label
-from PIL import Image, ImageTk
+import io
+import socket
+import struct
+import time
+import cv2
+import sys
+from PIL import Image
 
-class VideoClient:
-    def __init__(self, host_ip, port):
-        self.client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.client_socket.connect((host_ip, port))
-        self.data = b""
-        self.payload_size = struct.calcsize("Q")
-        
-    def receive_frame(self):
-        while True:
-            while len(self.data) < self.payload_size:
-                packet = self.client_socket.recv(4*1024) # 4K
-                if not packet:
-                    return None
-                self.data += packet
-            packed_msg_size = self.data[:self.payload_size]
-            self.data = self.data[self.payload_size:]
-            msg_size = struct.unpack("Q", packed_msg_size)[0]
-            
-            while len(self.data) < msg_size:
-                self.data += self.client_socket.recv(4*1024)
-            
-            frame_data = self.data[:msg_size]
-            self.data = self.data[msg_size:]
-            frame = pickle.loads(frame_data)
-            return frame
+client_socket = socket.socket()
+client_socket.connect(('localhost', 9999))
+connection = client_socket.makefile('wb')
 
-    def close(self):
-        self.client_socket.close()
+camera = cv2.VideoCapture(0)
 
-class VideoDisplay:
-    def __init__(self, video_client):
-        self.video_client = video_client
-        self.root = Tk()
-        self.root.title("Receiving Video")
-        self.label = Label(self.root)
-        self.label.pack()
-        self.update_frame()
-
-    def update_frame(self):
-        frame = self.video_client.receive_frame()
-        if frame is None:
-            print("No frame received. Exiting ...")
-            self.root.quit()
-            return
-        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-        img = Image.fromarray(frame)
-        imgtk = ImageTk.PhotoImage(image=img)
-        self.label.image = imgtk
-        self.label.configure(image=imgtk)
-        self.label.after(10, self.update_frame)
-
-    def start(self):
-        self.root.mainloop()
-
-if __name__ == "__main__":
-    host_ip = 'localhost' # paste your server ip address here
-    port = 9999
-    video_client = VideoClient(host_ip, port)
-    video_display = VideoDisplay(video_client)
-    video_display.start()
-    video_client.close()
+try:
+    while True:
+        ret, frame = camera.read()
+        if not ret:
+            break
+        img = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        img_pil = Image.fromarray(img)
+        image_stream = io.BytesIO()
+        img_pil.save(image_stream, format='JPEG')
+        image_stream.seek(0)
+        image_len = image_stream.getbuffer().nbytes
+        connection.write(struct.pack('<L', image_len))
+        connection.write(image_stream.read())
+        time.sleep(0.01)
+finally:
+    connection.write(struct.pack('<L', 0))
+    connection.close()
+    client_socket.close()
+    camera.release()
--- camera-server/CamServer.py
+++ camera-server/CamServer.py
@@ -1,59 +1,36 @@
-# Welcome to PyShine
+import io
+import socket
+import struct
+from PIL import Image
+import cv2
+import numpy
+import sys
 
-# This code is for the server 
-# Lets import the libraries
-import socket, cv2, pickle, struct, imutils
-import threading
-
-class VideoServer:
-    def __init__(self, host_ip, port):
-        self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.server_socket.bind((host_ip, port))
-        self.server_socket.listen(5)
-        
-        self.camera_connections = []
-        print("LISTENING AT:", (host_ip, port))
-    
-    def start_thread(self):
-        thread = threading.Thread(target=self._start)
-        thread.daemon = False
-        thread.start()
-
-    def _start(self):
+def handle_client(connection):
+    try:
         while True:
-            client_socket, addr = self.server_socket.accept()
-            print('GOT CONNECTION FROM:', addr)
-            if client_socket:
-                self.send_video(client_socket)
-    
-    def _connect_all_cameras(self):
-        i = 0
-        while True:
-            cap = cv2.VideoCapture(i)
-            if cap.isOpened():
-                self.camera_connections.append(cap)
-            else:
+            image_len = struct.unpack('<L', connection.read(struct.calcsize('<L')))[0]
+            if not image_len:
                 break
-            i += 1
-
-    def send_video(self, client_socket):
-        self._connect_all_cameras()
-        
-        while True:
-            img, frame = vid.read()
-            frame = imutils.resize(frame, width=350)
-            a = pickle.dumps(frame)
-            message = struct.pack("Q", len(a)) + a
-            client_socket.sendall(message)
-            
-            cv2.imshow('TRANSMITTING VIDEO', frame)
-            key = cv2.waitKey(1) & 0xFF
-            if key == ord('q'):
-                client_socket.close()
+            image_stream = io.BytesIO()
+            image_stream.write(connection.read(image_len))
+            image_stream.seek(0)
+            image = Image.open(image_stream)
+            im = cv2.cvtColor(numpy.array(image), cv2.COLOR_RGB2BGR)
+            cv2.imshow('Video', im)
+            if cv2.waitKey(1) & 0xFF == ord('q'):
                 break
+        cv2.destroyAllWindows()
+    finally:
+        connection.close()
 
-if __name__ == "__main__":
-    host_ip = 'localhost'
-    port = 9999
-    video_server = VideoServer(host_ip, port)
-    video_server.start_thread()
+server_socket = socket.socket()
+server_socket.bind(('localhost', 9999))  
+server_socket.listen(5)
+print("Listening on localhost:9999")
+
+while True:
+    connection = server_socket.accept()[0].makefile('rb')
+    handle_client(connection)
+
+server_socket.close()
